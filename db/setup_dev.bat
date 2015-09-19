1a416e7134e3b029d598bcf6f7ec616cd5bec970
codazon <codazongroup@gmail.com>
2015-09-19 18:25:30 +0700

    header và footer

--- db/setup_dev.bat
+++ db/setup_dev.bat
@@ -1,3 +1,3 @@
 C:\xampp\mysql\bin\mysqladmin -u root drop coffeecup
 C:\xampp\mysql\bin\mysqladmin -u root create coffeecup
-C:\xampp\mysql\bin\mysql -u root coffeecup < magento_sample_data_for_1.9.1.0.sql
+C:\xampp\mysql\bin\mysql -u root coffeecup < xproject-08-09.sql
